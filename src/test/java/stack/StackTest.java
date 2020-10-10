d907a23cddcdc3382d5465df179684f8f655b95b
ASHUTOSH <ashutosh@192.168.1.3>
2020-10-10 15:32:28 +0530

    [Ashutosh]Added:Stack Push

--- src/test/java/stack/StackTest.java
+++ src/test/java/stack/StackTest.java
@@ -10,7 +10,14 @@ public class StackTest {
 	public void Stack_push() {
 		System.out.println("Stack Push");
 
-
+		DataStruc<Integer> firstnodeadd = new DataStruc<Integer>(70);
+		DataStruc<Integer> secondnodeadd = new DataStruc<Integer>(30);
+		DataStruc<Integer> thirdnodeadd = new DataStruc<Integer>(56);
+		Stack stack = new Stack();
+		stack.push(firstnodeadd);
+		stack.push(secondnodeadd);
+		stack.push(thirdnodeadd);
+		stack.print();
 
 
 }
