fe4292100ff290f012ef8f9dad53ed1760014bee
ASHUTOSH <ashutosh@192.168.1.3>
2020-10-10 15:40:23 +0530

    [Ashutosh]Added:Peak and Pop

--- src/test/java/stack/StackTest.java
+++ src/test/java/stack/StackTest.java
@@ -19,6 +19,25 @@ public class StackTest {
 		stack.push(thirdnodeadd);
 		stack.print();
 
+	}
 
-}
+	@Test
+	public void Stack_peak_pop() {
+		System.out.println("Stack Peak and Pop");
+
+		DataStruc<Integer> firstnodeadd = new DataStruc<Integer>(70);
+		DataStruc<Integer> secondnodeadd = new DataStruc<Integer>(30);
+		DataStruc<Integer> thirdnodeadd = new DataStruc<Integer>(56);
+		Stack stack = new Stack();
+		stack.push(firstnodeadd);
+		stack.push(secondnodeadd);
+		stack.push(thirdnodeadd);
+		stack.print();
+
+		System.out.println("Peak is: " + stack.peak());
+		stack.print();
+		stack.pop();
+		stack.print();
+
+	}
 }
